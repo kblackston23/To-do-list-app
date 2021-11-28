7f01541ccb003565570a5a8f91e39d9a764fa9c4
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2021-11-27 21:58:06 -0600

    Updated stylings and added functions to js so app will work correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-let list = $('#list');
-let li = $('<li></li>');
-let input = $('#input');
-let inputValue = input.val();
+//Adding new item to List
 
 function newItem(){
+  let list = $('#list');
+  let li = $('<li></li>');
+  let input = $('#input');
+  let inputValue = $('#input').val();
+
   li.append(inputValue);
 
-  if (inputValue === ' '){
+  if (inputValue === ''){
     alert ('Cannot be blank!');
   }else {
-    list.append(li);
-    input.val();
+    $('#list').append(li);
+    $('#input').val('');
   }
-}
 
-$('#button').on('click', newItem);
+
+//Crossing out item from List
 
 function crossOut(){
+  li.toggleClass('strike');
+}
 
+li.on('dblclick', crossOut);
+
+//Add delete button
+let crossOutButton = $('<crossOutButton></crossOutButton>')
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+function deleteListItem(){
+  li.addClass('delete');
+}
+crossOutButton.on('click', deleteListItem);
+
+//Make list sortable
+
+$('#list').sortable()
 }
